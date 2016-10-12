9e218d37229d75ef2ae48a28011a9ec9c022937d
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2016-10-12 00:53:25 -0500

    Full support for Befunge-93
    :)

--- README.md
+++ README.md
@@ -2,27 +2,39 @@
 
 ## Supported Commands
 
-| Command | Description                                                  |
-| ------- | ------------------------------------------------------------ |
-| 0...f   | push numerical value                                         |
-| +       | pop a, b and push a + b                                      |
-| -       | pop a, b and push b - a                                      |
-| *       | pop a, b and push a * b                                      |
-| /       | pop a, b and push b / a                                      |
-| '       | push the next cell as an ascii                               |
-| "       | start string mode, push next cells as ascii up to the next " |
-| .       | pop numerical value from stack and print                     |
-| ,       | pop ascii value from stack and print                         |
-| :       | duplicate the top of the stack                               |
-| k       | pop n, repeat next command n times                           |
-| ^       | set instruction pointer delta to (0, 1)                      |
-| v       | set instruction pointer delta to (0, -1)                     |
-| <       | set instruction pointer delta to (-1, 0)                     |
-| >       | set instruction pointer delta to (1, 0)                      |
-| [       | turn the instruction pointer left relative to direction      |
-| ]       | turn the instruction pointer right relative to direction     |
-| r       | reverses the instruction pointer delta                       |
-| #       | jumps over the next cell                                     |
-| ;       | turns off execution until next ;                             |
-| j       | pop n, jump over the n next cells                            |
-| @       | halt                                                         |
+| Command | Description                                                         |
+| ------- | ------------------------------------------------------------------- |
+| 0...f   | push numerical value                                                |
+| +       | pop a, b and push a + b                                             |
+| -       | pop a, b and push b - a                                             |
+| *       | pop a, b and push a * b                                             |
+| /       | pop a, b and push b / a (integer division)                          |
+| %       | pop a, b and push b % a (modulus on integers)                       |
+| !       | pop a, if a is 0 push 1, else push 0 (logical not)                  |
+| `       | pop a, b and push b > a                                             |
+| '       | push the next cell as an ascii                                      |
+| "       | start string mode, push next cells as ascii up to the next "        |
+| .       | pop numerical value from stack and print                            |
+| ,       | pop ascii value from stack and print                                |
+| :       | duplicate the top of the stack                                      |
+| \       | swap the top two values on the stack                                |
+| $       | discard the top value on the stack                                  |
+| k       | pop n, repeat next command n times                                  |
+| ^       | set instruction pointer delta to (0, 1)                             |
+| v       | set instruction pointer delta to (0, -1)                            |
+| <       | set instruction pointer delta to (-1, 0)                            |
+| >       | set instruction pointer delta to (1, 0)                             |
+| [       | turn the instruction pointer left relative to direction             |
+| ]       | turn the instruction pointer right relative to direction            |
+| r       | reverses the instruction pointer delta                              |
+| ?       | sets the instruction pointer to a random dirction                   |
+| _       | horizontal if, pop a, if a = 0, move right, else move left          |
+| |       | vertical if, pop a, if a = 0, move down, else move  up              |
+| #       | jumps over the next cell                                            |
+| ;       | turns off execution until next ;                                    |
+| j       | pop n, jump over the n next cells                                   |
+| &       | get an integer from the user and push it                            |
+| ~       | get a character from the user and push it                           |
+| g       | pop y, x and push the character in the position (x, y), else push 0 |
+| p       | pop y, x, a and push the ascii value a into the position (x, y)     |
+| @       | halt                                                                |
--- src/cbi.c
+++ src/cbi.c
@@ -4,23 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include "cbi.h"
 #include "delta.h"
 #include "stack.h"
 
-#define MAKE_PRINT(X) X = (X < 0x20) ? 0x20 : X
-#define NUM(X) (((X) <= 0x39) ? (X) - 0x30 : 10 + (X) - 0x61)
-#define ABS(X) (((X) < 0) ? (-(X)) : (X))
-
-typedef enum ascii_mode_e {
-    OFF, SINGLE, MULTI
-} ascii_mode_t;
-
-int parse_command (char command, stack_t *stack, delta_t *delta) {
+int parse_command (stack_t *stack, delta_t *delta, instruction_space *is, int pos_row, int pos_col) {
     static ascii_mode_t ascii_mode = OFF;
     static int repeat = 1;
     static int skip_num = 0;
     static int toggle_exec = 0;
     static int jumping_back = 0;
+
+    char command = *(*((is->space) + pos_row) + pos_col);
 
     if (skip_num > 0) {
         skip_num--;
@@ -99,6 +95,25 @@ int parse_command (char command, stack_t *stack, delta_t *delta) {
                 a = pop (stack) / a;
                 push (stack, a);
                 break;
+            case '%':
+                a = pop (stack);
+                if (a == 0) {
+                    printf ("Error: division by zero.\n");
+                    return 1;
+                }
+                a = pop (stack) % a;
+                push (stack, a);
+                break;
+            case '!':
+                a = pop (stack);
+                a = (a) ? 0 : 1;
+                push (stack, a);
+                break;
+            case '`':
+                a = pop (stack);
+                a = (pop (stack) > a) ? 1 : 0;
+                push (stack, a);
+                break;
             case '\'':
                 ascii_mode = SINGLE;
                 break;
@@ -112,9 +127,20 @@ int parse_command (char command, stack_t *stack, delta_t *delta) {
                 printf ("%c", (char) pop (stack));
                 break;
             case ':':
-                a += pop (stack);
+                a = pop (stack);
                 push (stack, a);
                 push (stack, a);
+                break;
+            case '\\':
+                {
+                    int b = pop (stack);
+                    a = pop (stack);
+                    push (stack, b);
+                    push (stack, a);
+                }
+                break;
+            case '$':
+                pop (stack);
                 break;
             case 'k':
                 repeat = pop (stack);
@@ -141,6 +167,20 @@ int parse_command (char command, stack_t *stack, delta_t *delta) {
             case 'r':
                 reflect (delta);
                 break;
+            case '?':
+                a = rand () % 4;
+                (*(direction + a)) (delta);
+                break;
+            case '_':
+                a = pop (stack);
+                if (!a) right (delta);
+                else left (delta);
+                break;
+            case '|':
+                a = pop (stack);
+                if (!a) down (delta);
+                else up (delta);
+                break;
             case '#':
                 skip_num = 1;
                 break;
@@ -153,6 +193,45 @@ int parse_command (char command, stack_t *stack, delta_t *delta) {
                     reflect (delta);
                     skip_num = ABS(skip_num);
                     skip_num -= 2;
+                }
+                break;
+            case '&':
+                scanf ("%d", &a);
+                push (stack, a);
+                break;
+            case '~':
+                scanf ("%c", (char*) &a);
+                push (stack, (char) a);
+                break;
+            case 'g':
+                {
+                    a = pop (stack);
+                    int x = pop (stack);
+                    if (x >= is->num_cols || a >= is->num_rows) push (stack, 0);
+                    else push (stack, (char) *(*((is->space) + a) + x));
+                }
+                break;
+            case 'p':
+                {
+                    int y = pop (stack);
+                    int x = pop (stack);
+                    a = pop (stack);
+                    // If y is out of bounds, extend in the y direction
+                    if (y >= is->num_rows) {
+                        is->space = (char**) realloc (is->space, sizeof (char*) * (y + 1));
+                        for (int i = is->num_rows; i < (y + 1); i++) {
+                            *((is->space) + i) = (char*) calloc ((y + 1), 1);
+                        }
+                        is->num_rows = y + 1;
+                    }
+                    // If x is out of bounds, extend in the x direction
+                    if (x >= is->num_cols) {
+                        for (int i = 0; i < is->num_rows; i++) {
+                            *((is->space) + i) = (char*) realloc (*((is->space) + i), (x + 1));
+                        }
+                        is->num_cols = x + 1;
+                    }
+                    *(*((is->space) + y) + x) = (char) a;
                 }
                 break;
             case '@':
@@ -170,12 +249,17 @@ int parse_command (char command, stack_t *stack, delta_t *delta) {
 int main (int argc, char **argv) {
     char *filename; 
     FILE *in;
-    int num_rows, num_cols;
+    // REMOVE--------------------------------
+    //int num_rows, num_cols;
     int pos_row, pos_col;
-    char **instruction_space;
+    instruction_space *is;
     char current_read;
     stack_t *stack;
     delta_t delta;
+    
+    // Seed rand
+    time_t t;
+    srand ((unsigned) time (&t));
 
     // Load file
     // File not in args
@@ -196,12 +280,13 @@ int main (int argc, char **argv) {
     }
 
     // Initialize the instruction space to be a single spot
-    num_rows = 1;
-    num_cols = 1;
+    is = (instruction_space*) malloc (sizeof (instruction_space));
+    is->num_rows = 1;
+    is->num_cols = 1;
+    is->space = (char**) malloc (sizeof (char*));
+    *((is->space) + 0) = (char*) calloc (is->num_cols, 1);
     pos_row = 0;
     pos_col = 0;
-    instruction_space = (char**) malloc (sizeof (char*));
-    *(instruction_space + 0) = (char*) calloc (num_cols, 1);
 
     // Each char is read into here
     // Used for testing the char
@@ -210,33 +295,33 @@ int main (int argc, char **argv) {
     // Reads until the end of the file
     while ((current_read = fgetc (in)) != EOF) {
         // If we have a line longer than the previous max, extend them all out one
-        if (pos_col == num_cols) {
-            num_cols++;
-            for (int row = 0; row < num_rows; row++) {
-                *(instruction_space + row) = (char*) realloc (*(instruction_space + row), num_cols);
+        if (pos_col == is->num_cols) {
+            (is->num_cols)++;
+            for (int row = 0; row < is->num_rows; row++) {
+                *((is->space) + row) = (char*) realloc (*((is->space) + row), is->num_cols);
             }
         }
 
         // Add the instruction into the instruction space
-        *(*(instruction_space + pos_row) + pos_col) = current_read;
+        *(*((is->space) + pos_row) + pos_col) = current_read;
         pos_col++;
 
         // If there is a newline, add a new row
         if (current_read == '\n') {
-            num_rows++;
-            //printf ("%i\n", num_rows);
-            instruction_space = (char**) realloc (instruction_space, sizeof (char*) * num_rows);
-            *(instruction_space + (num_rows - 1)) = (char*) calloc (num_cols, 1);
+            (is->num_rows)++;
+            //printf ("%i\n", is->num_rows);
+            is->space = (char**) realloc (is->space, sizeof (char*) * is->num_rows);
+            *((is->space) + (is->num_rows - 1)) = (char*) calloc (is->num_cols, 1);
             pos_row++;
             pos_col = 0;
         }
     }
 
     // Make nonprintables whitespace
-    for (int r = 0; r < num_rows; r++) {
-        for (int c = 0; c < num_cols; c++) {
-            MAKE_PRINT(*(*(instruction_space + r) + c));
-            //printf ("0x%02X ", *(*(instruction_space + r) + c));
+    for (int r = 0; r < is->num_rows; r++) {
+        for (int c = 0; c < is->num_cols; c++) {
+            MAKE_PRINT(*(*((is->space) + r) + c));
+            //printf ("0x%02X ", *(*((is->space) + r) + c));
         }
         //printf ("\n");
     }
@@ -251,24 +336,25 @@ int main (int argc, char **argv) {
     pos_col = 0;
 
     // Run through instruction space
-    while (!(parse_command (*(*(instruction_space + pos_row) + pos_col), stack, &delta))) {
-        //printf ("%c\n", *(*(instruction_space + pos_row) + pos_col));
+    while (!(parse_command (stack, &delta, is, pos_row, pos_col))) {
+        //printf ("%c\n", *(*((is->space) + pos_row) + pos_col));
         pos_col += delta.delta_x;
         pos_row += delta.delta_y;
-        if (pos_col < 0) pos_col = num_cols - 1;
-        if (pos_col == num_cols) pos_col = 0;
-        if (pos_row < 0) pos_row = num_rows - 1;
-        if (pos_row == num_rows) pos_row = 0;
+        if (pos_col < 0) pos_col = is->num_cols - 1;
+        if (pos_col == is->num_cols) pos_col = 0;
+        if (pos_row < 0) pos_row = is->num_rows - 1;
+        if (pos_row == is->num_rows) pos_row = 0;
     }
 
     // Release everything from memory
     fclose (in);
 
     free (filename);
-    for (int cx = 0; cx < num_rows; cx++) {
-        free (*(instruction_space + cx));
+    for (int cx = 0; cx < is->num_rows; cx++) {
+        free (*((is->space) + cx));
     }
-    free (instruction_space);
+    free (is->space);
+    free (is);
 
     clear (stack);
     free (stack);
